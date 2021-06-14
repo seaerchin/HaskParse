197db7a38a28bdf68ffabb432375106ef9c309fb
seaerchin <jer005@e.ntu.edu.sg>
2021-06-15 04:04:03 +0800

    feat(parsers): added new integer parser

--- src/Text/Parsers/String.hs
+++ src/Text/Parsers/String.hs
@@ -2,14 +2,18 @@ module Text.Parsers.String where
 
 import Lib
 import Text.Combinators.Base
+import Text.Combinators.List
 import Text.Parsers.Base
 
 parseString :: String -> Parser String
 parseString s =
   map parseChar s
     |> foldr f (return "")
-
-f :: Parser a -> Parser [a] -> Parser [a]
-f cur acc = cur `andThen` acc |> fmap asSingleParser
   where
-    asSingleParser (a, b) = a : b
+    f :: Parser a -> Parser [a] -> Parser [a]
+    f cur acc = cur `andThen` acc |> fmap asSingleParser
+      where
+        asSingleParser (a, b) = a : b
+
+parseInteger :: Parser Int
+parseInteger = many1 anyDigit |> fmap read
